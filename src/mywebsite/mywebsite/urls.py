e76942743ee1f2f087112e71eaf1ba8ad9507c98
Jeta Sunanda <sunandajeta@gmail.com>
2025-04-26 04:05:50 +0700

    membuat app

--- src/mywebsite/mywebsite/urls.py
+++ src/mywebsite/mywebsite/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from blog import views as blogViews
+from about import views as aboutViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', views.about),
+    path('blog/', blogViews.index),
+    path('about/', aboutViews.index),
     path('', views.index),
 ]
--- src/mywebsite/mywebsite/views.py
+++ src/mywebsite/mywebsite/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 # method view index
 def index(request):
     return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
